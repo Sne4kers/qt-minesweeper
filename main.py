5af082435d7abcbd051c31a9aafe60eda138e1fe
Yehor Kozyr <yehor.kozyr@gmail.com>
2022-04-13 00:43:26 +0200

    First commit!  Changes to be committed: 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from PyQt6.QtWidgets import QApplication, QPushButton, QMainWindow
+from PyQt6.QtCore import Qt, QSize
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("First application")
+
+        button = QPushButton("Click")
+
+        self.setFixedSize(QSize(400, 300))
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+# Start the event loop.
+app.exec()
